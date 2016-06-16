1f48f6c8b10d38ee775a1bb844aefa065098d4b6
zapalap <przemyslaw.zapala@gmail.com>
2016-06-16 20:39:24 +0200

    Initial commit

---
+++ Cha.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cha.Api.WebApiApplication" Language="C#" %>
---
+++ Cha.Core/ChaEngine.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cha.Core
+{
+    public class ChaEngine
+    {
+        private static ActorSystem Actors;
+
+        private ChaEngine() { }
+
+        public static ActorSystem ActorSystem
+        {
+            get
+            {
+                if (Actors == null)
+                {
+                    Actors = ActorSystem.Create("ChaActorSystem");
+                }
+
+                return Actors;
+            }
+        }
+    }
+}
